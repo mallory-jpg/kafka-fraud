c3da21169354c0847773e9111ceb21b7bec3680b
mallory-jpg <65197541+mallory-jpg@users.noreply.github.com>
2021-11-18 14:39:45 -0600

    hardcode transactions_per_second

--- generator/app.py
+++ generator/app.py
@@ -2,12 +2,13 @@ from time import sleep
 import os
 from transactions import create_random_transaction
 import json
-from kafka import KafkaProducer #TODO no module found 'kafka'
+from kafka import KafkaProducer #TODO https://github.com/mallory-jpg/kafka-fraud/issues/2 no module found 'kafka'
 
 KAFKA_VERSION = os.environ.get("KAFKA_VERSION")
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 TRANSACTIONS_TOPIC = os.environ.get("TRANSACTIONS_TOPIC")
-TRANSACTIONS_PER_SECOND = float(os.environ.get("TRANSACTIONS_PER_SECOND")) # 1000
+# float(os.environ.get("TRANSACTIONS_PER_SECOND"))
+TRANSACTIONS_PER_SECOND = 1000
 SLEEP_TIME = 1 / TRANSACTIONS_PER_SECOND
 
 if __name__ == "__main__":
